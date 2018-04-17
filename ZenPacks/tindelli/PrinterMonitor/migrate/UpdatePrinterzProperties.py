80b39757de1c49f63551a6d85b566898d9e6290e
Tom Indelli <tindelli@gmail.com>
2018-04-17 16:29:04 -0500

    rework device-level reclassification

--- ZenPacks/tindelli/PrinterMonitor/migrate/UpdatePrinterzProperties.py
+++ ZenPacks/tindelli/PrinterMonitor/migrate/UpdatePrinterzProperties.py
@@ -19,12 +19,6 @@ from Products.ZenModel.ZenPack import ZenPackMigration
 
 from ZenPacks.tindelli.PrinterMonitor.PrinterDevice import PrinterDevice
 
-try:
-    from Products.ZenUtils.events import pausedAndOptimizedReindexing
-except ImportError:
-    def pausedAndOptimizedReindexing():
-        yield
-
 class UpdatePrinterzProperties(ZenPackMigration):
     """Update zProperties for /Printer device class."""
 
@@ -65,45 +59,14 @@ class UpdatePrinterzProperties(ZenPackMigration):
             "Reclassifying devices as %s",
             TO_zPythonClass)
 
-#       with pausedAndOptimizedReindexing:
-#           for device in deviceclass.getSubDevicesGen():
-#               if not isinstance(device, PrinterDevice):
-#                   reclass_device(device, PrinterDevice)
+        for device in deviceclass.getSubDevicesGen():
+            if not isinstance(device, PrinterDevice):
+                deviceclass.moveDevices(
+                    device.deviceClass().getOrganizerName(),
+                    device.id)
 
-#                   device.buildRelations()
+                device.buildRelations()
 
         LOG.info("Finished converting devices.")
-
-
-#def reclass_device(device, klass):
-    """Change the __class__ of device to klass.
-
-    This isn't quite as simple as "device.__class__ = klass" because ZODB
-    stores the class in the persistent reference to the object, not in the
-    object itself. As soon as the container is loaded from the ZODB, a ghost is
-    created for its persistent reference using the class mentioned there.
-
-    This function works around this issue by also replacing the persistent
-    reference to the object after changing it's __class__.
-
-    See the following article for the inspiration for this function.
-
-    http://blog.redturtle.it/2013/02/25/migrating-dexterity-items-to-dexterity-containers
-
-    """
-    # For example: dmd.Devices.Server.SSH.Linux.devices._objects
-#    container = device.getPrimaryParent()._objects
-
-    # Get the unwrapped object.
-#    unwrapped_device = container[device.id]
-
-    # Delete the object's persistent reference.
-#    del container[unwrapped_device.id]
-
-    # Change the unwrapped object's class.
-#    unwrapped_device.__class__ = klass
-
-    # Add back a persistent reference to the newly-classed object.
-#    container[unwrapped_device.id] = unwrapped_device
 
 UpdatePrinterzProperties()
