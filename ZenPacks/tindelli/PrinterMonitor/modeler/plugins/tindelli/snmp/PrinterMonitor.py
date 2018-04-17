abe16e9e286e890d1b472fb526ea46b40d05764e
Tom Indelli <tindelli@gmail.com>
2018-04-17 16:38:29 -0500

    re-add marker technology

--- ZenPacks/tindelli/PrinterMonitor/modeler/plugins/tindelli/snmp/PrinterMonitor.py
+++ ZenPacks/tindelli/PrinterMonitor/modeler/plugins/tindelli/snmp/PrinterMonitor.py
@@ -3,16 +3,51 @@ from Products.DataCollector.plugins.DataMaps import ObjectMap, RelationshipMap
 
 class PrinterMonitor(SnmpPlugin):
 
+    ###########################################################################
+    # Text Conversion definitions from RFC 3805
+    ###########################################################################
+
+    # The type of marking technology used for this marking subunit
+    PrtMarkerMarkTechTC = {
+        1: 'other',
+        2:  'unknown',
+        3:  'LED',                              # electrophotographicLED
+        4:  'Laser',                            # electrophotographicLaser
+        5:  'Other Electrophotographic',        # electrophotographicOther
+        6:  '9-pin Dot Matrix',                 # impactMovingHeadDotMatrix9pin
+        7:  '24-pin Dot Matrix',                # impactMovingHeadDotMatrix24pin
+        8:  'Other Dot Matrix',                 # impactMovingHeadDotMatrixOther
+        9:  'Impact Moving Head Fully Formed',  # impactMovingHeadFullyFormed
+        10: 'Impact Band',                      # impactBand
+        11: 'Impact Other',                     # impactOther
+        12: 'Inkjet (Aqueous)',                 # inkjetAqueous
+        13: 'Inkjet (Solid)',                   # inkjetSolid
+        14: 'Inkjet (Other)',                   # inkjetOther  
+        15: 'Pen',
+        16: 'Thermal Transfer',                 
+        17: 'Thermal Sensitive',
+        18: 'Thermal Diffusion',
+        19: 'Thermal Other',
+        20: 'Electroerosion',
+        21: 'Electrostatic',
+        22: 'Photographic Microfiche',
+        23: 'Photographic Imagesetter',
+        24: 'Photographic Other',
+        25: 'Ion Deposition',
+        26: 'eBeam',
+        27: 'Typesetter',
+    }
+
     # Basic SNMP attribute maps
     snmpGetMap = GetMap({
         '.1.3.6.1.2.1.43.5.1.1.17.1': 'prtGeneralSerialNumber',
-#        '.1.3.6.1.2.1.43.10.2.1.2.1.1': 'prtMarkerMarkTech',
+        '.1.3.6.1.2.1.43.10.2.1.2.1.1': 'prtMarkerMarkTech',
         '.1.3.6.1.2.1.43.10.2.1.6.1.1': 'numberColors',
     })
 
     def process(self, device, results, log):
         log.info('processing %s for device %s', self.name(), device.id)
-        getdata, tabledata = results
+        getData, tableData = results
 
         maps = []
 
@@ -25,8 +60,8 @@ class PrinterMonitor(SnmpPlugin):
 
         maps.append(ObjectMap(
             data={
-                'colorSupport': True if getData['numberColors'] else False,
-                'prtMarkerMarkTech': PrtMarkerMarkTechTC[getdata['prtMarkerMarkTech']]
+                'colorSupport': True if getdata['numberColors'] else False,
+                'prtMarkerMarkTech': PrtMarkerMarkTechTC[getData['prtMarkerMarkTech']]
             }
         ))
 
